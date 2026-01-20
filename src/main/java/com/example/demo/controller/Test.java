01bef35dbad9a49f204565f4bf1efd74e1697a40
jizhongcheng <jizhongcheng@494145>
2026-01-20 10:25:43 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/api/v1/test")
+public class Test {
+
+    // 确保有这个映射
+    @GetMapping("/getName")
+    public String getName(String name){
+        System.out.println(name);
+        return "test==" + name;
+    }
+
+
+}
